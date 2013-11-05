1968d1959596c34cce83d5c89ab5ed6cecf2247b
Juliën Hanssens <j@hanssens.com>
2013-11-05 11:34:26 +0100

    Initial checkin of clean, project
    - With fully functional task list overview
    - Based on backbone.js

---
+++ source/BackboneSandbox/Controllers/TasksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackboneSandbox.Controllers
+{
+    public class TasksController : Controller
+    {
+
+        /// <summary>
+        /// Lists all tasks
+        /// </summary>
+        public ActionResult Overview()
+        {
+            return View();
+        }
+	}
+}
